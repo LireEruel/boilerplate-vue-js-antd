b3bfbef38c695afa7fc92afae2649bbf7a1e71c6
LireEruel <jsh001505@naver.com>
2023-10-23 15:22:37 +0900

    오타 수정

--- src/src/features/users/api/addUser.js
+++ src/src/features/users/api/addUser.js
@@ -1,9 +1,10 @@
-import commonAxios from "./../../../lib/coomAxios";
+import commonAxios from "../../../lib/commonAxios";
 export const addUsers = (id, name) => {
   const params = {
     id: id,
     name: name,
   };
+
   try {
     const resp = commonAxios.get("/users", params);
     return resp.data;
